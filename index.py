7445fa54dbdfeb367339503adf2d34df5addb182
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2024-01-26 23:30:03 -0800

    trying to optimize to get solution as close as possible

--- index.py
+++ index.py
@@ -2,8 +2,17 @@ import tensorflow as tf
 import numpy as np 
 import matplotlib.pyplot as plt
 
-X = np.array([-10, -5, 0, 5, 10, 15, 20, 25])
-y = np.array([2, 7, 12, 17, 22, 27, 32, 37])
+# X = np.array([-10, -5, 0, 5, 10, 15, 20, 25])
+X = tf.range(-100,100,5)
+# print(len(X))
+X_train = X[:40]
+X_test = X[40:]
+y = X + 12
+y_train = y[:40]
+y_test = y[40:]
+
+print(X, y)
+# y = np.array([2, 7, 12, 17, 22, 27, 32, 37])
 
 print(y == X + 12)
 
@@ -14,6 +23,7 @@ tf.random.set_seed(42)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(100, activation="relu"),
+    tf.keras.layers.Dense(100, activation="relu"),
     # tf.keras.layers.Dense(100, activation=None),
     tf.keras.layers.Dense(1)
 ])
@@ -22,11 +32,12 @@ model.compile(loss=tf.keras.losses.mae,
     # optimizer=tf.keras.optimizers.SGD(),
     # optimizer=tf.keras.optimizers.Adam(),
     # optimizer=tf.keras.optimizers.Adam(lr=.01),
-    optimizer=tf.keras.optimizers.Adam(lr=.00001),
+    optimizer=tf.keras.optimizers.Adam(lr=.0001),
+    # optimizer=tf.keras.optimizers.Adam(learning_rate=.00001),              
     metrics=["mae"]
 )
 
-model.fit(tf.expand_dims(X, axis=-1), y, epochs = 100)
+model.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs = 100, verbose=0)
 
 # print(model.predict([17]))
 
